2ed0f7425d4db5322bbae3b1a21fc60c69b88272
Harshit-Maurya838 <44harshitmaurya@gmail.com>
2025-11-30 13:50:25 +0530

    Setup of TaskContext or API for Task related functions

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { TasksProvider } from "./context/TasksContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <TasksProvider>
+        <App />
+      </TasksProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
